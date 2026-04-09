82f9f5daffc57164aa92d0cfd24ab07be73d27d2
med0viy <gullsssrranga1658@gmail.com>
2026-04-09 18:58:45 +0300

    fixes

--- cmd/todoapp/main.go
+++ cmd/todoapp/main.go
@@ -8,6 +8,7 @@ import (
 	"syscall"
 	"time"
 
+	core_config "github.com/med0viy/practika/internal/core/config"
 	core_logger "github.com/med0viy/practika/internal/core/logger"
 	core_pgx_pool "github.com/med0viy/practika/internal/core/repository/postgres/pool/pgx"
 	core_http_maddleware "github.com/med0viy/practika/internal/core/transport/http/middleware"
@@ -21,12 +22,9 @@ import (
 	"go.uber.org/zap"
 )
 
-var (
-	timeZone = time.UTC
-)
-
 func main() {
-	time.Local = timeZone
+	cfg := core_config.NewConfigMust()
+	time.Local = cfg.TimeZone
 
 	ctx, cancel := signal.NotifyContext(
 		context.Background(),
@@ -41,7 +39,7 @@ func main() {
 	}
 	defer logger.Close()
 
-	logger.Debug("application nime zone", zap.Any("zone", timeZone))
+	logger.Debug("application nime zone", zap.Any("zone", time.Local))
 
 	logger.Debug("initiazling postgres connection pool")
 	pool, err := core_pgx_pool.NewPool(

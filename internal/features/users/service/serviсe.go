fda7b57aa35c7b69b96fa9c2093806a2faf6601c
med0viy <gullsssrranga1658@gmail.com>
2026-04-15 15:57:16 +0300

    Merge conflict resolved in service.go

--- internal/features/users/service/serviсe.go
+++ internal/features/users/service/serviсe.go
@@ -6,13 +6,8 @@ import (
 	"github.com/med0viy/practika/internal/core/domain"
 )
 
-<<<<<<< HEAD:internal/features/users/service/servise.go
-type UsersServise struct {
-	usersRepository usersRepository
-=======
 type UsersService struct {
-	usersRepository UsersRepository
->>>>>>> master:internal/features/users/service/serviсe.go
+	usersRepository usersRepository
 }
 
 type usersRepository interface {
@@ -23,7 +18,7 @@ type usersRepository interface {
 	PatchUser(ctx context.Context, id int, user domain.User) (domain.User, error)
 }
 
-func NewUsersService(usersRepository UsersRepository) *UsersService {
+func NewUsersService(usersRepository usersRepository) *UsersService {
 	return &UsersService{
 		usersRepository: usersRepository,
 	}
